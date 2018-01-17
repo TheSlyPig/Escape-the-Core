d3c16d8fd57a0ad8161320782356dc3a5c788724
Kegan Cunningham <kcunningham04@hotmail.com>
2018-01-17 17:43:58 -0500

    Add diagonal enclosing lines

--- bundle.js
+++ bundle.js
@@ -169,7 +169,8 @@ class Game {
 
   render(ctx) {
     if (this.i > 75) {
-      this.lines = [new __WEBPACK_IMPORTED_MODULE_0__line_js__["a" /* default */](ctx, this.gameCanvas, Math.floor((Math.random() * 2) + 1))];
+      // this.lines = [new Line(ctx, this.gameCanvas, 3)];
+      this.lines = [new __WEBPACK_IMPORTED_MODULE_0__line_js__["a" /* default */](ctx, this.gameCanvas, Math.floor((Math.random() * 4) + 1))];
       this.i = 0;
     } else {
       this.i += 1;
@@ -211,6 +212,12 @@ class Line {
     } else if (type === 2) {
       this.x = this.gameCanvas.height;
       this.y = this.gameCanvas.width;
+    } else if (type === 3) {
+      this.x = this.gameCanvas.width - 20;
+      this.y = 0;
+    } else if (type === 4) {
+      this.x = 0;
+      this.y = this.gameCanvas.height - 20;
     }
 
   }
@@ -234,6 +241,14 @@ class Line {
       this.x = this.x - 3;
       this.y = this.y - 3;
       this.lineWidth = this.lineWidth - 3.2;
+    } else if (this.type === 3) {
+      this.x = this.x - 3;
+      this.y = this.y + 3;
+      this.lineWidth = this.lineWidth - 3.2;
+    } else if (this.type === 4) {
+      this.x = this.x + 3;
+      this.y = this.y - 3;
+      this.lineWidth = this.lineWidth - 3.2;
     }
   }
 
@@ -241,8 +256,14 @@ class Line {
     ctx.beginPath();
     ctx.strokeStyle = this.color;
     ctx.lineWidth = this.lineWidth;
-    ctx.moveTo(this.x, this.y);
-    ctx.lineTo(this.x - 10, this.y - 10);
+    if (this.type === 1 || this.type === 2) {
+      ctx.moveTo(this.x, this.y);
+      ctx.lineTo(this.x + 10, this.y + 10);
+    } else if (this.type === 3 || this.type === 4) {
+      ctx.moveTo(this.x + 20, this.y);
+      ctx.lineTo(this.x + 10, this.y + 10);
+    }
+
     ctx.closePath();
     ctx.stroke();
   }
