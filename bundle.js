7284eed4b4d16ffb27ffc76bfc9d7168052b2f32
Kegan Cunningham <kcunningham04@hotmail.com>
2018-01-17 18:02:07 -0500

    Add horizontal and vertical lines

--- bundle.js
+++ bundle.js
@@ -169,8 +169,7 @@ class Game {
 
   render(ctx) {
     if (this.i > 75) {
-      // this.lines = [new Line(ctx, this.gameCanvas, 3)];
-      this.lines = [new __WEBPACK_IMPORTED_MODULE_0__line_js__["a" /* default */](ctx, this.gameCanvas, Math.floor((Math.random() * 4) + 1))];
+      this.lines = [new __WEBPACK_IMPORTED_MODULE_0__line_js__["a" /* default */](ctx, this.gameCanvas, Math.floor((Math.random() * 8) + 1), 'red')];
       this.i = 0;
     } else {
       this.i += 1;
@@ -194,10 +193,10 @@ class Game {
 
 "use strict";
 class Line {
-  constructor(ctx, gameCanvas, type) {
+  constructor(ctx, gameCanvas, type, color) {
     this.ctx = ctx;
     this.gameCanvas = gameCanvas;
-    this.color = this.randomColor();
+    this.color = color;
     this.x = 0;
     this.y = 0;
     this.lineWidth = 300;
@@ -218,38 +217,59 @@ class Line {
     } else if (type === 4) {
       this.x = 0;
       this.y = this.gameCanvas.height - 20;
+    } else if (type === 5) {
+      this.x = 0;
+      this.y = (this.gameCanvas.height / 2) - 10;
+    } else if (type === 6) {
+      this.x = this.gameCanvas.width;
+      this.y = (this.gameCanvas.height / 2) - 10;
+    } else if (type === 7) {
+      this.x = (this.gameCanvas.width / 2) - 10;
+      this.y = 0;
+    } else if (type === 8) {
+      this.x = (this.gameCanvas.width / 2) - 10;
+      this.y = this.gameCanvas.height
     }
 
   }
-
-  randomColor() {
-    const HEX_DIGITS = '0123456789ABCDEF';
-    let color = '#';
-    for (let i = 0; i < 6; i++) {
-      color += HEX_DIGITS[Math.floor((Math.random() * 16))];
-    }
-
-    return color;
-  }
+  // 
+  // randomColor() {
+  //   const HEX_DIGITS = '0123456789ABCDEF';
+  //   let color = '#';
+  //   for (let i = 0; i < 6; i++) {
+  //     color += HEX_DIGITS[Math.floor((Math.random() * 16))];
+  //   }
+  //
+  //   return color;
+  // }
 
   closeIn() {
     if (this.type === 1) {
       this.x = this.x + 3;
       this.y = this.y + 3;
-      this.lineWidth = this.lineWidth - 3.2;
     } else if (this.type === 2) {
       this.x = this.x - 3;
       this.y = this.y - 3;
-      this.lineWidth = this.lineWidth - 3.2;
     } else if (this.type === 3) {
       this.x = this.x - 3;
       this.y = this.y + 3;
-      this.lineWidth = this.lineWidth - 3.2;
     } else if (this.type === 4) {
       this.x = this.x + 3;
       this.y = this.y - 3;
-      this.lineWidth = this.lineWidth - 3.2;
+    } else if (this.type === 5) {
+      this.x = this.x + 3;
+      this.y = this.y;
+    } else if (this.type === 6) {
+      this.x = this.x - 3;
+      this.y = this.y;
+    } else if (this.type === 7) {
+      this.x = this.x;
+      this.y = this.y + 3;
+    } else if (this.type === 8) {
+      this.x = this.x;
+      this.y = this.y - 3;
     }
+    this.lineWidth = this.lineWidth - 3.2;
   }
 
   render(ctx) {
@@ -261,6 +281,12 @@ class Line {
       ctx.lineTo(this.x + 10, this.y + 10);
     } else if (this.type === 3 || this.type === 4) {
       ctx.moveTo(this.x + 20, this.y);
+      ctx.lineTo(this.x + 10, this.y + 10);
+    } else if (this.type === 5 || this.type === 6) {
+      ctx.moveTo(this.x, this.y + 10);
+      ctx.lineTo(this.x + 10, this.y + 10);
+    } else if (this.type === 7 || this.type === 8) {
+      ctx.moveTo(this.x + 10, this.y);
       ctx.lineTo(this.x + 10, this.y + 10);
     }
 
